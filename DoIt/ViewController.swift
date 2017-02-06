cf2a62dab6a2b0c65b89c3cf464dd6112a657eaf
Ron Sala <ron.sala@gmail.com>
2017-02-05 23:52:08 -0500

    L58: Task Class

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by RONALD SALA on 2/5/17.
 //  Copyright © 2017 RON SALA. All rights reserved.
+//  Class exercise for Nick Walter's "How to Build a Freaking iPhone App"
 //
 
 import UIKit
@@ -11,23 +12,50 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        tasks = makeTasks()
         
         tableView.dataSource = self
         tableView.delegate = self
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Hello"
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️\(task.name)"
+        } else {
+        cell.textLabel?.text = task.name
+        }
+        
         return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the Dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Mow the lawn"
+        task3.important = false
+        
+        return [task1, task2, task3]
     }
     
 }
---
+++ DoIt/task.swift
@@ -0,0 +1,15 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by RONALD SALA on 2/5/17.
+//  Copyright © 2017 RON SALA. All rights reserved.
+//  Class exercise for Nick Walter's "How to Make a Freaking iPhone App"
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
+
