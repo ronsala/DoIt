ffcef260cdc020480fa7cfff2c082058fa1e8b22
Ron Sala <ron.sala@gmail.com>
2017-02-06 01:25:17 -0500

    L60: Setting things up

---
+++ CreateTaskViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by RONALD SALA on 2/6/17.
+//  Copyright © 2017 RON SALA. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var TaskNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+    }
+
