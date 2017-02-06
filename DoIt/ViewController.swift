8ed644076ed08890432537ea47bba7fe1a8409b6
Ron Sala <ron.sala@gmail.com>
2017-02-06 00:31:00 -0500

    L59: Segue

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -57,6 +57,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         return [task1, task2, task3]
     }
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
     
 }
 
